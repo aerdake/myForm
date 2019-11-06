5052aa228d05f680607f097e4d42ccafafcd7007
wangfengduo <wangfengduo@lingtu.com>
2019-11-06 10:17:24 +0800

    目录结构

---
+++ src/main/java/com/example/service/Impl/MenuServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.service.Impl;
+
+/**
+ * @ClassName MenuServiceImpl.java
+ * @Description TODO
+ * @Author 王凤铎
+ * @Date 2019/11/6 10:16
+ * @Version 1.0
+ */
+public class MenuServiceImpl {
+}
